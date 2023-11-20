c846ebd8c7afd4c621ff67fbd1f44f1a6620323a
eloydiazmunoz <eloydiazlsgi@gmail.com>
2023-11-20 12:55:55 +0100

    20/11/2023/12:55

--- EDM_PROG_INTRODUCING GUESS MY NUMBER/EDM_PROG_INTRODUCING GUESS MY NUMBER.cpp
+++ EDM_PROG_INTRODUCING GUESS MY NUMBER/EDM_PROG_INTRODUCING GUESS MY NUMBER.cpp
@@ -1,37 +1,37 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <iostream>  // Inclusió de la llibreria d'entrada/sortida
+#include <cstdlib>  // Inclusió de la llibreria estàndard de C amb funcions d'ús general
+#include <ctime>  // Inclusió de la llibreria per a manipulació del temps
 
-using namespace std;
+using namespace std;  // Espai de noms per a utilitzar les entitats de la llibreria standard
 
-int main() {
-    srand(time(0)); // Inicialització de la generació de nombres aleatoris
+int main() {  // Funció principal del programa
+    srand(time(0));  // Inicialització de la generació de nombres aleatoris
 
-    int numeroAleatori = rand() % 100 + 1; // Genera un nombre aleatori entre 1 i 100
-    int suposicio, intents = 0;
+    int numeroAleatori = rand() % 100 + 1;  // Genera un nombre aleatori entre 1 i 100
+    int suposicio, intents = 0;  // Declaració de variables per emmagatzemar la suposició de l'usuari i el nombre d'intents
 
-    cout << "Benvingut al joc de l'endevina el nombre entre 1 i 100!" << endl;
+    cout << "Benvingut al joc de l'endevina el nombre entre 1 i 100!" << endl;  // Missatge de benvinguda
 
     do {
-        cout << "Introdueix la teva suposicio: ";
-        cin >> suposicio;
+        cout << "Introdueix la teva suposicio: ";  // Sol·licitud de la suposició de l'usuari
+        cin >> suposicio;  // Lectura de la suposició de l'usuari
 
-        if (suposicio < 1 || suposicio > 100) {
-            cout << "La suposicio ha d'estar entre 1 i 100. Torna a provar." << endl;
-            continue;
+        if (suposicio < 1 || suposicio > 100) {  // Comprovació de la validesa de la suposició
+            cout << "La suposicio ha d'estar entre 1 i 100. Torna a provar." << endl;  // Missatge d'error
+            continue;  // Torna al principi del bucle
         }
 
-        if (suposicio > numeroAleatori) {
-            cout << "Massa alta. Torna a provar." << endl;
+        if (suposicio > numeroAleatori) {  // Comparació de la suposició amb el nombre aleatori
+            cout << "Massa alta. Torna a provar." << endl;  // Missatge indicant que la suposició és massa alta
         }
-        else if (suposicio < numeroAleatori) {
-            cout << "Massa baixa. Torna a provar." << endl;
+        else if (suposicio < numeroAleatori) {  // Comparació de la suposició amb el nombre aleatori
+            cout << "Massa baixa. Torna a provar." << endl;  // Missatge indicant que la suposició és massa baixa
         }
 
-        intents++;
-    } while (suposicio != numeroAleatori);
+        intents++;  // Increment del comptador d'intents
+    } while (suposicio != numeroAleatori);  // Bucle que continua fins que la suposició coincideixi amb el nombre aleatori
 
-    cout << "Felicitats! Has endevinat el nombre en " << intents << " intents." << endl;
+    cout << "Felicitats! Has endevinat el nombre en " << intents << " intents." << endl;  // Missatge de felicitació amb el nombre d'intents
 
-    return 0;
+    return 0;  // Indicació d'èxit del programa
 }
